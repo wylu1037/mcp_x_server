4c8935ee749e8ccfba79868c9a7cf82255227ef9
wylu1037 <wylu1037@gmail.com>
2025-04-17 19:45:12 +0800

    feat: use pydantic_ai

--- agents/tweet_agent.py
+++ agents/tweet_agent.py
@@ -1,8 +1,12 @@
 from textwrap import dedent
-from agno.agent import Agent
-from agno.models.deepseek import DeepSeek
-from agents.tools.create_tweet import create_tweet
 from dotenv import load_dotenv 
+from pytwitter import Api
+from pytwitter.models.tweet import Tweet
+from dotenv import load_dotenv
+from os import getenv
+from pydantic_ai import Agent, RunContext
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.deepseek import DeepSeekProvider
 
 load_dotenv()
 
@@ -26,19 +30,20 @@ load_dotenv()
 """
 agent = Agent(
     name="Tweet Agent",
-    description="An agent that enhances and expands user-provided text or topics before tweeting.",
-    model=DeepSeek(),
-    tools=[create_tweet],
+    model=OpenAIModel(
+        model_name="deepseek-chat",
+        provider=DeepSeekProvider(api_key=getenv("DEEPSEEK_API_KEY"))
+    ),
+    system_prompt="An agent that enhances and expands user-provided text or topics before posting on X.",
     instructions=dedent("""\
         You are a tweet agent that takes user-provided text or topics, 
         enhances or expands the content, and call the appropriate tools to post a tweet.
-                        
+
         Attention:
         - The tweet should be in English.
         - The tweet should be concise and to the point.
         - The tweet should be engaging and interesting.
         - The tweet should be related to the user-provided text or topics.
-        - The tweet should be no more than 280 characters.
         - The tweet content only contains the pure text, no markdown or other formatting.
 
         Tools:
@@ -46,5 +51,31 @@ agent = Agent(
 
         Finally, you should call the create_tweet tool to post the tweet.
     """),
-    show_tool_calls=True
 )
+
+api = Api(
+    consumer_key=getenv("CONSUMER_KEY"),
+    consumer_secret=getenv("CONSUMER_SECRET"),
+    access_token=getenv("ACCESS_TOKEN"),
+    access_secret=getenv("ACCESS_SECRET")
+)
+
+@agent.tool
+def create_tweet(_: RunContext[None], text: str) -> str:
+    """
+    Use this tool to create a tweet
+
+    Args:
+        text (str): The text of the tweet
+
+    Returns:
+        str: Data for tweet created(JSON format).
+    """
+    response = api.create_tweet(text=text)
+    if response is None:
+        return "Error: Failed to create tweet"
+    elif isinstance(response, Tweet):
+        return f"Tweet created successfully: id is {response.id}"
+    else:
+        return "Error: Failed to create tweet"
+    
--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 from fastmcp import FastMCP
 from agents.tweet_agent import agent
-
+from pydantic_ai.tools import RunContext
 mcp = FastMCP("X MCP 🚀", description="A simple MCP server")
 
 @mcp.tool()
-def create_tweet(text: str) -> str:
+async def create_tweet(text: str) -> str:
     """Create a tweet"""
     # Ignore the return value
-    agent.run(text) 
-    return "Tweet created successfully"
+    r = await agent.run(text) 
+    return r.output
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
