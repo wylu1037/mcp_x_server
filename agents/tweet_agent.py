8d7e9cca621396e2b471c26436d977fd33773d08
wylu1037 <wylu1037@gmail.com>
2025-04-17 18:20:45 +0800

    feat: use the deepseek model

--- agents/tweet_agent.py
+++ agents/tweet_agent.py
@@ -1,23 +1,37 @@
 from textwrap import dedent
 from agno.agent import Agent
-from agno.models.openai import OpenAILike
+from agno.models.deepseek import DeepSeek
 from agents.tools.create_tweet import create_tweet
 from dotenv import load_dotenv 
-from os import getenv
 
 load_dotenv()
 
-agent = Agent(
-    name="Tweet Agent",
-    description="An agent that enhances and expands user-provided text or topics before tweeting.",
+"""
+    This is a tweet agent.
+    You can specify other models, like OpenAI or SiliconFlow.
+
+    ```python
+    from agno.models.deepseek import DeepSeek
+    from agno.models.openai import OpenAILike
+    from os import getenv
+
     model=OpenAILike(
         id="deepseek-ai/DeepSeek-R1-Distill-Qwen-14B", 
         api_key=getenv("SILICONFLOW_API_KEY"), 
         base_url="https://api.siliconflow.cn/v1",
     ),
+    
+    # model=DeepSeek(),
+    ```
+"""
+agent = Agent(
+    name="Tweet Agent",
+    description="An agent that enhances and expands user-provided text or topics before tweeting.",
+    model=DeepSeek(),
     tools=[create_tweet],
     instructions=dedent("""\
-        You are a tweet agent that takes user-provided text or topics, enhances or expands the content, and call the appropriate tools to post a tweet.
+        You are a tweet agent that takes user-provided text or topics, 
+        enhances or expands the content, and call the appropriate tools to post a tweet.
                         
         Attention:
         - The tweet should be in English.
